141e68add86a590d82d86d6dbb4d15ba609d57d3
tomitahajime <ponponpon816@gmail.com>
2021-07-15 23:31:12 +0900

    [Start]2章【学習の準備をしよう】プログラム作成、コンパイル、プログラム実行

---
+++ Ready01.java
@@ -0,0 +1,10 @@
+// Javaは大文字と小文字が区別されるので、間違いなく入力する。
+public class Ready01 {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
+
+// Javaのソースファイルをコンパイルするには「javac」コマンドを使用する。(javac Ready01.java)
+// コンパイルが成功すると、同じディレクトリにReady01.classというファイルが作成される。このファイルをクラスファイルと呼ぶ。
+// 作成されたクラスファイルを実行する。Javaプログラムを実行するには「java」コマンドを使用する。(java Ready01)
