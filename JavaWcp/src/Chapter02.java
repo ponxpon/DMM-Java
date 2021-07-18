5c21c92e11636619531a840c127ae0bcded6c1d2
tomitahajime <ponponpon816@gmail.com>
2021-07-18 23:54:23 +0900

    [Finish]3章、二つの文字列を出力

--- JavaWcp/src/Chapter02.java
+++ JavaWcp/src/Chapter02.java
@@ -1,6 +1,7 @@
-
+// 3章 確認問題
 public class Chapter02 {
 	public static void main(String[] args) {
-        System.out.println("こんにちは!");
-    }
+		System.out.println("Hello WEBCAMP!");
+		System.out.println("Hello Java!");
+	}
 }
