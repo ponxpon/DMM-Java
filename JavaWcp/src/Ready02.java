212bfb6b6761e5cb2879a9c9e9333ce9a17a8dc4
tomitahajime <ponponpon816@gmail.com>
2021-07-18 23:48:12 +0900

    [Start]3章【Javaプログラミングの仕方と実行方法を学ぼう】ブロックと文、コーティング規約の把握

--- JavaWcp/src/Ready02.java
+++ JavaWcp/src/Ready02.java
@@ -1,6 +1,14 @@
-
+// ↓ クラスブロックの始まり
 public class Ready02 {
+	//	↓ メソッドブロックの始まり (インデントしてわかりやすくする)
 	public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+		//		↓ コメントを追加 (メソッドブロックよりさらにインデントしてわかりやすくする)
+		//		System.out.println はJavaの標準出力用メソッド。後に続く括弧内の内容を文字列として出力する。
+		//		文字列(Hello World!)を出力
+		System.out.println("Hello World!"); // ← 文字列を出力する処理
+	}
+	//	↑ メソッドブロックの終わり
 }
+// ↑ クラスブロックの終わり
+
+// コードフォーマット(shift + command + F)で規約に沿ったソースコードに整形
