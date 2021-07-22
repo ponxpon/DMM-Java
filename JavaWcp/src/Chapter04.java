dabc6d0b4de3025031094dbc525de88c64983236
tomitahajime <ponponpon816@gmail.com>
2021-07-22 23:57:05 +0900

    [Finish]4章、確認問題

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,15 @@
+// 4章　確認問題
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		final int LUCKY_NUMBER = 7;
+		// 「String」型の変数「str」の宣言と初期化して出力
+		String str = "DMM WEBCAMP";
+		System.out.println(str);
+		// 変数「str」に再代入して出力
+		str = "まろやかバナナミルク";
+		System.out.println(str);
+		// int」型で宣言したクラス定数を出力
+		System.out.println(LUCKY_NUMBER);
+	}
+}
