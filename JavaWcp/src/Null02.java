150cb7e2df342af76d1465b0b6e95c4d766875e1
tomitahajime <ponponpon816@gmail.com>
2021-07-24 17:31:33 +0900

    [Add]参照型

--- JavaWcp/src/Null02.java
+++ JavaWcp/src/Null02.java
@@ -1,4 +1,18 @@
+// 5章　参照型
 
 public class Null02 {
-
+	public static void main(String[] args) {
+		// String型は文字列を扱うことができるクラス
+		String string1;
+		// ”あいう”の文字数を出力
+		string1 = "あいう";
+		System.out.println(string1 + "の文字数：" + string1.length());
+		// ””（空文字）の文字数を出力
+		string1 = "";
+		System.out.println(string1 + "の文字数：" + string1.length());
+		// nullとは参照型変数の中身が何もないことを表現する特別な値
+		// nullの変数を参照すると通称「ヌルポ」と呼ばれる「NullPointerException」のエラー
+		string1 = null;
+		System.out.println(string1 + "の文字数：" + string1.length());
+	}
 }
