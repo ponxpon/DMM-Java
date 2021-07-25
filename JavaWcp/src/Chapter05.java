cdc00aedfcb22562f167aef06b9b9c98a5b07176
tomitahajime <ponponpon816@gmail.com>
2021-07-25 16:09:33 +0900

    [Finish]5章、確認問題

---
+++ JavaWcp/src/Chapter05.java
@@ -0,0 +1,29 @@
+// 5章　確認問題
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		// プリミティブ型
+		long long1 = 1111111111111111111L;
+		System.out.println(long1);
+		
+		double double1 = 1234567.89;
+		System.out.println(double1);
+		
+		boolean boolean1 = true;
+		System.out.println(boolean1);
+		
+		// ラッパークラス
+		Float float1 = 0.123f;
+		System.out.println(float1);
+		
+		Integer integer1 = 1; 
+		System.out.println(integer1);
+		
+		Character character1 = 'あ';
+		System.out.println(character1);
+		
+		// 配列
+		int[] array1 = { 0, 1, 2, 3, 4 };
+		System.out.println(array1[3]);
+	}
+}
