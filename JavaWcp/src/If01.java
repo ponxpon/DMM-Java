2f6842de3946010ce03f01574ceb66749b789390
tomitahajime <ponponpon816@gmail.com>
2021-07-31 22:18:17 +0900

    [Start]7章【条件分岐を学ぼう】

---
+++ JavaWcp/src/If01.java
@@ -0,0 +1,31 @@
+// 7章【条件分岐を学ぼう】
+
+// if文とは、指定した条件により処理を分岐する制御文
+public class If01 {
+	public static void main(String[] args) {
+		int number = 7;
+		// if文
+		// numberが5未満の場合、ブロックの処理を実行
+		if (number < 5) {
+			// 「◯は5未満です」を出力
+			System.out.println(number + " は5未満です");
+		}
+		// if文
+		// numberが10未満の場合、ブロックの処理を実行
+		if (number < 10) {
+			// 「◯は10未満です」を出力
+			System.out.println(number + " は10未満です"); // 7 は10未満です
+		}
+		// if文
+		// numberが15未満の場合、ブロックの処理を実行
+		if (number < 15) {
+			// 「◯は15未満です」を出力
+			System.out.println(number + " は15未満です"); // 7 は15未満です
+		}
+	}
+}
+
+// if文の基本構文
+// if (条件式) {
+//    ブロック(処理文);
+// }
