64a9cf0155f87d57992668f1bfca8285c12cda16
tomitahajime <ponponpon816@gmail.com>
2021-08-03 23:18:42 +0900

    [Finish]7章、確認問題2

--- JavaWcp/src/Chapter07.java
+++ JavaWcp/src/Chapter07.java
@@ -1,7 +1,8 @@
-// 7章 確認問題1
+// 7章 確認問題
 
 public class Chapter07 {
 	public static void main(String[] args) {
+		// 確認問題1
 		int number = 5;
 		// 5km以下
 		if (number <= 5) {
@@ -15,6 +16,26 @@ public class Chapter07 {
 		// 15kmより長い
 		} else {
 			System.out.println("とても遠いです");
+		}
+
+		// 確認問題2
+		String color = "red";
+		// // switch文：分岐処理の開始
+		switch (color) {
+		// 値がredの場合にブロックを実行
+		case "red":
+			System.out.println("赤信号です");
+			break;
+		// 値がyellowの場合にブロックを実行
+		case "yellow":
+			System.out.println("黄信号です");
+			break;
+		// 値がblueの場合にブロックを実行
+		case "blue":
+			System.out.println("青信号です");
+			break;
+		default:
+			System.out.println("信号の色ではありません");
 		}
 	}
 }
