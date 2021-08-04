d8184290b652ae91dd96a0b6cfc2976dbafe6e4d
tomitahajime <ponponpon816@gmail.com>
2021-08-04 23:00:27 +0900

    [Start]8章【繰り返しを学ぼう】

---
+++ JavaWcp/src/While01.java
@@ -0,0 +1,19 @@
+// 8章【繰り返しを学ぼう】
+
+public class While01 {
+	public static void main(String[] args) {
+		int number = 1;
+		// while文とは、指定の条件が「true」の間、繰り返し処理を行なう制御文
+		// numberが50未満の場合、ブロックの処理を行なう。
+		while (number < 50) {
+			// numberにnumber * 2を代入して、numberを出力
+			number *= 2;
+			System.out.println("While01 = " + number);
+		}
+	}
+}
+
+// 基本構文
+//	while (条件式) {
+//	    ブロック（繰り返し処理）
+//	}
