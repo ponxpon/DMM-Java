ed208bcf36b67bfabf2ea128ee97da39d2c57805
tomitahajime <ponponpon816@gmail.com>
2021-08-16 00:29:19 +0900

    [Update]パッケージ名を指定したプログラム

--- JavaWcp/src/classMethod/ClassMethod02.java
+++ JavaWcp/src/classMethod/ClassMethod02.java
@@ -1,11 +1,12 @@
+// 9章【クラスとメソッドを理解しよう】
 // パッケージ宣言
-package classMethod.human;
+package classMethod;
 
-// インポート文を記載してclassMethod.human.Human02クラスを参照できるようにする。
+// インポート文
 import classMethod.human.Human02;
 
 public class ClassMethod02 {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         Human02 yamada = new Human02();  // Human02クラスのインスタンスを生成
         System.out.println("名前は" + yamada.name + "で、年齢は" + yamada.age + "です。");
 
