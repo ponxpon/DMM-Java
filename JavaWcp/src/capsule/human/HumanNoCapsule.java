e0a82311f8cf615b6d75264e79bacb5e5053031c
tomitahajime <ponponpon816@gmail.com>
2021-08-29 06:59:35 +0900

    [Start]12章【カプセル化を理解しよう】

---
+++ JavaWcp/src/capsule/human/HumanNoCapsule.java
@@ -0,0 +1,12 @@
+// 12章【カプセル化を理解しよう】
+package capsule.human;
+
+public class HumanNoCapsule {
+	public String name;
+	public int age;
+
+	public HumanNoCapsule(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+}
