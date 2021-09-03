189190e98ae8ef42b0ce567daced3f112c3a3d3d
tomitahajime <ponponpon816@gmail.com>
2021-09-03 16:04:14 +0900

    [Add]確認問題

---
+++ JavaWcp/src/practice/NoCapsuleMan.java
@@ -0,0 +1,13 @@
+// 12章【カプセル化を理解しよう】確認問題
+
+package practice;
+
+public class NoCapsuleMan {
+	public String name;
+	public int age;
+
+	public NoCapsuleMan(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+}
