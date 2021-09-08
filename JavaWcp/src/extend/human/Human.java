a8e9d07aebbb08b288683a6c44aa5d888ab3d021
tomitahajime <ponponpon816@gmail.com>
2021-09-08 18:07:57 +0900

    [Start]13章【継承を理解しよう】

---
+++ JavaWcp/src/extend/human/Human.java
@@ -0,0 +1,21 @@
+// 13章【継承を理解しよう】
+
+package extend.human;
+
+public class Human {
+	private String name;
+    protected int age;
+
+    public Human(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+}
