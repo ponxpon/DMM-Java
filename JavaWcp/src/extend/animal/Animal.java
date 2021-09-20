84b54b1bebac66774008db380bf89f91a4cd5e7c
tomitahajime <ponponpon816@gmail.com>
2021-09-21 03:55:37 +0900

    [Add]サンプルプログラムの説明コメント

--- JavaWcp/src/extend/animal/Animal.java
+++ JavaWcp/src/extend/animal/Animal.java
@@ -1,16 +1,16 @@
 // 13章【継承を理解しよう】
 
-// 抽象クラスとは、抽象メソッドを1つ以上持つクラスのこと。
-// 抽象メソッドとは、処理を記述せずにメソッド名や引数、戻り値だけを定義したメソッドのこと。
-
 package extend.animal;
 
+// abstract修飾子を設定して、抽象クラスとして宣言
 public abstract class Animal {
 	protected String name;
 
+	// 共通メソッドを記述
 	public void eat() {
 		System.out.println(name + "が食事をしました！");
 	}
 
+	// サブクラスごとに違う処理は、抽象メソッドにしてサブクラスに実装させる。
 	public abstract void cry();
 }
--- JavaWcp/src/extend/animal/Dog.java
+++ JavaWcp/src/extend/animal/Dog.java
@@ -2,12 +2,25 @@
 
 package extend.animal;
 
+//抽象クラスとは、抽象メソッドを1つ以上持つクラスのこと。
+//抽象メソッドとは、処理を記述せずにメソッド名や引数、戻り値だけを定義したメソッドのこと。
+
+// 抽象クラスのAnimalクラスを継承
 public class Dog extends Animal {
 	public Dog() {
         this.name = "犬";
     }
 
+	// 抽象メソッドを上書きして処理を記述
     public void cry() {
         System.out.println(this.name + "が吠えました。ワン！ワン！");
     }
 }
+
+// 基本構文
+// 抽象クラスの宣言
+// 「abstract」修飾子を指定してクラスを宣言することで抽象クラスになる。
+// アクセス修飾子 abstract class クラス名 {
+   // 抽象メソッドの宣言
+   // アクセス修飾子 abstract 戻り値の型 メソッド名(引数);
+// }
