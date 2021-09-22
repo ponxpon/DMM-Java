c75556c3f9b076ea96e84e68317d632b3b30647d
tomitahajime <ponponpon816@gmail.com>
2021-09-22 12:37:44 +0900

    [Add]インターフェース基本構文

--- JavaWcp/src/extend/Phone.java
+++ JavaWcp/src/extend/Phone.java
@@ -10,3 +10,11 @@ public interface Phone {
 
     public void call(String number);
 }
+
+// 基本構文
+//	[public] interface インターフェース名 extends インターフェース名, インターフェース名 {
+		// フィールドの宣言（定数）
+//	    (public static final) 定数の型 定数名 = 初期値;
+		// メソッドの宣言（抽象メソッド）
+//	    (public abstract) 戻り値の型 メソッド名(引数);
+//	}
