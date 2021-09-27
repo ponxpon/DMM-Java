7867a7817a36746aefb7de27401c0d8db521462d
tomitahajime <ponponpon816@gmail.com>
2021-09-27 12:36:47 +0900

    [Finish]13章【継承を理解しよう】確認問題

--- JavaWcp/src/practice/FlyingPhone.java
+++ JavaWcp/src/practice/FlyingPhone.java
@@ -5,5 +5,26 @@ package practice;
 import extend.phone.Phone;
 
 public class FlyingPhone implements Phone, Flying {
-    // プログラムを作成
+	private int minutes;
+
+	// 「int」型の引数を持つコンストラクタを定義。引数でメンバ変数を初期化。
+    public FlyingPhone(int minutes) {
+        this.minutes = minutes;
+    }
+
+    // Phoneインターフェースのメソッドをオーバーライド
+    public void call(String number) {
+        System.out.println(number + " に電話します。通話できるのは飛んでいる間だけです。");
+    }
+
+    // Flyingインターフェースのメソッドをオーバーライド
+    public void fly() {
+        System.out.println(minutes + " 分間、飛びます。 ");
+    }
+
+    // 「Phone」と「Flying」の両インターフェースにdefaultメソッドがあるのでオーバーライドする必要がある。
+    public void powerOff() {
+    	// Flyingのdefaultメソッドを呼び出す
+        Flying.super.powerOff();
+    }
 }
