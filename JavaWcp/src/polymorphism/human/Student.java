5f440b13905eff9b59a834651320b37501db539d
tomitahajime <ponponpon816@gmail.com>
2021-09-29 23:29:29 +0900

    [Add]サンプルプログラム

---
+++ JavaWcp/src/polymorphism/human/Student.java
@@ -0,0 +1,18 @@
+// 14章【ポリモーフィズムを理解しよう】
+
+package polymorphism.human;
+
+public class Student extends Human {
+	private int score;
+
+    public Student(String name, int age, int score) {
+        super(name, age);
+        this.score = score;
+    }
+
+    public String getProfile() {
+        String profile = "年齢は" + super.age + "です。";
+        profile += "学生で、テストの点数は" + this.score + "点です。";
+        return profile;
+    }
+}
