93f0cb22485fe24ef94ea156bec2c51085a81434
tomitahajime <ponponpon816@gmail.com>
2021-10-02 04:45:05 +0900

    [Start]15章【演習問題（番外編）】

---
+++ JavaWcp/src/practice/Chapter15_1.java
@@ -0,0 +1,17 @@
+// 15章【演習問題（番外編）】再帰の問題
+// 問題:最大公約数を「ユークリッドの互除法」で算出する再帰メソッドを呼び出して、以下の実行結果になるように「Chapter15_1.java」を作成する。
+
+package practice;
+
+public class Chapter15_1 {
+	public static void main(String[] args) {
+        int a = 976;
+        int b = 427;
+        System.out.println(a + " と " + b + " の最大公約数は、" + gcd(a, b) + "です。");
+    }
+
+    // ユークリッドの互除法で最大公約数を計算するメソッド
+    static int gcd(int a, int b) {
+        ・・・
+    }
+}
