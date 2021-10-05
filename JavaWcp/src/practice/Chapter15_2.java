70ca8abd3d83d38a7762443ddf1ff168d7562a98
tomitahajime <ponponpon816@gmail.com>
2021-10-05 23:05:38 +0900

    [Add]問題に使用するプログラム

---
+++ JavaWcp/src/practice/Chapter15_2.java
@@ -0,0 +1,47 @@
+// 15章【演習問題（番外編）】ファイル入出力の問題
+// 問題：JavaWcpの直下に「resources」ディレクトリ作成して、ディレクトリに以下の「input.txt」ファイルを作成する。
+// 作成した「input.txt」ファイルの読み込んで「output.txt」ファイルを出力するプログラム「Chapter15_2.java」を完成する。
+
+package practice;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Chapter15_2 {
+	public static void main(String[] args) {
+        FileSystem fs = FileSystems.getDefault();
+        // PathクラスのインスタンスをFileSystem.getPathメソッドで生成
+        Path inputPath = fs.getPath("resources/input.txt");
+        Path outputPath = fs.getPath("resources/output.txt");
+
+        // Java7以降に追加された「try-with-resources」文で処理
+        // Filesクラスを使用して、BufferedReaderクラスのインスタンスを生成
+        // Filesクラスを使用して、BufferedWriterクラスのインスタンスを生成
+        try (・・・) {
+
+            String line = null;
+            // BufferedReader.readLineメソッドを使用して、1行づつ処理
+            // 読み込む行がなくなるまでループ
+            while (・・・) {
+                // 文字数が5以外の場合はスキップ
+                ・・・
+                // 読み込んだ1行を出力
+                ・・・
+                // 改行がついてないので改行を出力
+                ・・・
+            }
+
+        // 例外処理を記述
+        // printStackTraceで例外を出力
+        } catch (・・・)  {
+            ・・・
+        } finally {
+            System.out.println("出力処理終了");
+        }
+    }
+}
