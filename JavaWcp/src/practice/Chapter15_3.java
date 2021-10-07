b2b599542cd7d3f499610131437417fc0eadee14
tomitahajime <ponponpon816@gmail.com>
2021-10-08 04:03:13 +0900

    [Add]確認問題3に使用するプログラム

---
+++ JavaWcp/src/practice/Chapter15_3.java
@@ -0,0 +1,25 @@
+// 15章【演習問題（番外編）】プリミティブ型のキャストの問題
+// 問題：指定された実行結果になるように、コンパイルエラーのプログラム「Chapter15_3.java」にキャストを追加する。
+
+package practice;
+
+public class Chapter15_3 {
+	public static void main(String[] args) {
+		int i1 = 123;
+		double d1 = i1;
+		System.out.println(d1);
+
+		double d2 = 12.345;
+		int i2 = d2;
+		System.out.println(i2);
+
+		int i3 = 1;
+		int i4 = 2;
+		double d3 = i3 / i4;
+		System.out.println(d3);
+
+		int i5 = 128;
+		byte b1 = i5;
+		System.out.println(b1);
+	}
+}
