baa3dd00dce6effe42ca9172c1698776fe892b36
tomitahajime <ponponpon816@gmail.com>
2021-10-11 01:48:18 +0900

    [Finish]確認問題4

--- JavaWcp/src/practice/ThreadSample.java
+++ JavaWcp/src/practice/ThreadSample.java
@@ -2,15 +2,25 @@
 
 package practice;
 
-public class ThreadSample {
+public class ThreadSample implements Runnable {
 	int num;
 
-    // コンストラクタ
+	// コンストラクタ
     public ThreadSample(int num) {
         this.num = num;
     }
 
-    // runメソッドを作成
+    // Runnableインターフェースを実装したクラスは、runメソッドを実装する必要がある。
     // Chapter15_4と同じようにループごとに1秒間スリープして表示
-    ・・・
+    public void run() {
+        for (int i = 1; i <= 3; i++) {
+            try {
+                // Thread.sleepで1秒間スリープ
+                Thread.sleep(1000);
+                System.out.println("ThreadSample" + num + "のスレッド : " + i + "回目");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
