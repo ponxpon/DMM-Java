93430a48eb730a188d337eaff1346b8de8f524ec
tomitahajime <ponponpon816@gmail.com>
2021-10-10 23:47:51 +0900

    [Add]スレッドの問題に使用するプログラム

---
+++ JavaWcp/src/practice/ThreadSample.java
@@ -0,0 +1,16 @@
+// 15章【演習問題（番外編）】スレッドの問題
+
+package practice;
+
+public class ThreadSample {
+	int num;
+
+    // コンストラクタ
+    public ThreadSample(int num) {
+        this.num = num;
+    }
+
+    // runメソッドを作成
+    // Chapter15_4と同じようにループごとに1秒間スリープして表示
+    ・・・
+}
