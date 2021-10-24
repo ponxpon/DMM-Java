bdb6ce7d6fd5d7de1ee5422a5598375ff8e1b619
tomitahajime <ponponpon816@gmail.com>
2021-10-24 23:42:29 +0900

    [Add]列挙型の特徴：変数として使える

--- JavaWcp/src/Dessert.java
+++ JavaWcp/src/Dessert.java
@@ -1,5 +1,8 @@
 // 2章【列挙型を理解しよう】書き方
 
 public enum Dessert {
-	APPLE, ICE_CREAM, CAKE,
+	APPLE, ICE_CREAM, CAKE;
+	static void printDessertName(Dessert dessert){
+	    System.out.println(dessert);
+	}
 }
---
+++ JavaWcp/src/Dessert01.java
@@ -0,0 +1,8 @@
+// 2章【列挙型を理解しよう】列挙型の特徴
+
+public enum Dessert01 {
+	// 列挙型もほかのクラスと同じように、変数に代入して使うことができる。
+	// Dessert apple = Dessert.APPLE;
+	// Dessert iceCream = Dessert.ICE_CREAM;
+	// Dessert cake = Dessert.CAKE;
+}
