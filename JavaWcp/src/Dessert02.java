3ca4fad9c52553be4ef79eced7d514f14c8d2471
tomitahajime <ponponpon816@gmail.com>
2021-10-31 23:45:23 +0900

    [Add]列挙型の機能：valueOf(String name)

--- JavaWcp/src/Dessert02.java
+++ JavaWcp/src/Dessert02.java
@@ -3,4 +3,9 @@
 public class Dessert02 {
 	// name()：列挙した定数名そのものをString型で返す。
 	// System.out.println(Dessert.APPLE.name());
+
+	// valueOf(String name)：name()とは逆に、String型の名称から列挙型のインスタンスを取得したい場合は、valueOf()を使う。
+	// Dessert apple = Dessert.valueOf("APPLE");
+	// System.out.println(apple);
+	// valueOf()に列挙されていない文字列を指定すると例外が発生する。
 }
