61450e43d4d39d829397bdb7864aeeaf3a935e20
tomitahajime <ponponpon816@gmail.com>
2021-11-01 23:24:21 +0900

    [Add]列挙型の機能：ordinal()

--- JavaWcp/src/Dessert.java
+++ JavaWcp/src/Dessert.java
@@ -1,5 +1,13 @@
 // 2章【列挙型を理解しよう】書き方
 
 public enum Dessert {
-	APPLE, ICE_CREAM, CAKE,
+	APPLE, ICE_CREAM, CAKE;
+
+	// 列挙型の機能
+	// ordinal()：列強した順序をint型で返す。（0スタート）
+	public static void main(String[] args) {
+		System.out.println(Dessert.APPLE.ordinal());
+		System.out.println(Dessert.ICE_CREAM.ordinal());
+		System.out.println(Dessert.CAKE.ordinal());
+	}
 }
