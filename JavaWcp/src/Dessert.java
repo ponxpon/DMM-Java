c9a757fd066bf3cd4d448b9772896555ec50e44b
tomitahajime <ponponpon816@gmail.com>
2021-11-03 00:01:03 +0900

    [Add]列挙型のさらなる活用：値を追加できる

--- JavaWcp/src/Dessert.java
+++ JavaWcp/src/Dessert.java
@@ -1,7 +1,26 @@
 // 2章【列挙型を理解しよう】書き方
 
 public enum Dessert {
-	APPLE, ICE_CREAM, CAKE;
+	// APPLE, ICE_CREAM, CAKE;
+
+	// 列挙型のさらなる活用
+	// 値を追加できる：列挙型は、列挙したそれぞれのインスタンスにさらに値を持たせることができる。
+
+	// それぞれに値段を指定する
+    APPLE(100), ICE_CREAM(200), CAKE(300),;
+
+  // 値段を保持するフィールド
+    private int price;
+
+  // コンストラクタを追加。引数はint型の値段。
+    private Dessert(int price) {
+        this.price = price;
+    }
+
+  // 値段を取得するメソッドを追加
+    public int getPrice() {
+        return price;
+    }
 
 	// 列挙型の機能
 	// ordinal()：列強した順序をint型で返す。（0スタート）
