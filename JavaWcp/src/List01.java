76da477930bfdb88f6b10cbc722b91f61c2cbacc
tomitahajime <ponponpon816@gmail.com>
2021-11-08 23:42:41 +0900

    [Add]Listの操作：要素数の取得、要素の削除

--- JavaWcp/src/List01.java
+++ JavaWcp/src/List01.java
@@ -15,23 +15,37 @@ public class List01 {
 	// LinkedListを生成する場合
 	List<Integer> list2 = new LinkedList<Integer>();
 
-	// Listの操作
+	public void main(String[] args) {
 
-	// 要素を追加する
-	// list1.add("りんご");
-	// list1.add("みかん");
+		// Listの操作
+		// 要素を追加する
+		list1.add("りんご");
+		list1.add("みかん");
 
-	// 位置を指定して要素を追加する
-	// list1.add(1, "バナナ");
+		// 位置を指定して要素を追加する
+		list1.add(1, "バナナ");
 
-	// 要素の取得：getメソッドを使用
-	// 要素を取得する
-	// list1.get(0);
+		// 要素の取得：getメソッドを使用
+		// 要素を取得する
+		list1.get(0);
 
-	// 要素の存在チェック：containsメソッドを使用
-	// 存在チェック (trueを返す)
-	// list1.contains("バナナ");
+		// 要素の存在チェック：containsメソッドを使用
+		// 存在チェック (trueを返す)
+		list1.contains("バナナ");
 
-	// 存在チェック (falseを返す)
-	// list1.contains("なし");
+		// 存在チェック (falseを返す)
+		list1.contains("なし");
+
+		// 要素数の取得：sizeメソッドを使用
+		list1.size(); // 3を返す
+
+		// 要素の削除：removeメソッドを使用
+		// 要素を削除 (要素が文字列の場合、文字列として同じであれば削除される)
+		list1.remove("バナナ");
+		list1.remove("なし"); // 何もしない
+
+		// 位置を指定して要素を削除する。
+		list1.remove(0);
+		list1.remove(5); // コレクションの要素数より大きい位置を指定すると実行時エラー
+	}
 }
