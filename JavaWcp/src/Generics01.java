6e1a7611f8e77f8d1bfdb7dbc8496932063c3f3e
tomitahajime <ponponpon816@gmail.com>
2021-11-30 00:11:17 +0900

    [Add]コレクションでのジェネリクスの使い方

--- JavaWcp/src/Generics01.java
+++ JavaWcp/src/Generics01.java
@@ -8,10 +8,18 @@ public class Generics01 {
 	List<String> items = new ArrayList<String>();
 
 	public void main(String[] args) {
+		// Stringが型パラメータとして指定されているため、String型要素は追加できるが、それ以外を追加しようとするとコンパイルエラーになる。
 		items.add("bag"); // OK
 
 		// これ以下はString型ではない要素を追加しているのでコンパイルエラー
 		// items.add(0);
 		// items.add(true);
+
+		// 要素を取り出して使う際も、String型であることが保証されているため、異なる型の変数に代入することはできない。
+		String item = items.get(0); // OK
+
+		// これ以下はString型ではない変数に代入しているのでコンパイルエラー
+		// int item = items.get(0);
+		// boolean item = items.get(0);
 	}
 }
